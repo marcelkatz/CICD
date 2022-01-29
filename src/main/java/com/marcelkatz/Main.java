2476f60f439463b8343c5ccb3130b25f2accf3f3
marcelkatz <marcel_katz@yahoo.com>
2022-01-28 21:53:44 -0500

    Change from "User1" to "User2" in System.out.println("User2: " + u2.getFirstName() + " " + u2.getLastName() + " " + u2.getPhoneNumber());

--- src/main/java/com/marcelkatz/Main.java
+++ src/main/java/com/marcelkatz/Main.java
@@ -5,6 +5,6 @@ public class Main {
         User u1 = new User("Omar", "Gomez", "12345678901");
         System.out.println("User1: " + u1.getFirstName() + " " + u1.getLastName() + " " + u1.getPhoneNumber());
         User u2 = new User("Jamil", "Jamil", "32145678901");
-        System.out.println("User1: " + u2.getFirstName() + " " + u2.getLastName() + " " + u2.getPhoneNumber());
+        System.out.println("User2: " + u2.getFirstName() + " " + u2.getLastName() + " " + u2.getPhoneNumber());
     }
 }
